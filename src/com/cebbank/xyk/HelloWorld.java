e1a319de867b8fe51796a3a447d9c1cb0ebf5719
gaozhan <1044896762@qq.com>
2017-01-11 11:00:38 +0800

    first commit 第一次提交Hello World

---
+++ src/com/cebbank/xyk/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.cebbank.xyk;
+
+/**
+ * Created by Administrator on 2017/1/11.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
